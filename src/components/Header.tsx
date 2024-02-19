a61b1d2160c95b9bc1e2024d56f76bd5d524d96b
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-02-19 23:53:58 +0000

    add Header Component

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Header.module.css'
+
+export function Header() {
+  return (
+    <header>
+        <strong className={styles.header}>Feed</strong>
+    </header>
+  )
+}
