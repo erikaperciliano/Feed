2b6897700004cc8ca777731f5dd60f558f0494b3
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-02-22 19:05:14 +0000

    creates Post Component

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,15 +1,31 @@
 import React from 'react'
+import styles from './Post.module.css'
 
-interface PostProps {
-    author: string;
-    content: string;
-}
-
-export function Post({ author, content }: PostProps) {
+export function Post() {
   return (
-    <div>
-      <strong>{author}</strong>
-      <p>{content}</p>
-    </div>
+    <article className={styles.post}>
+        <header>
+            <div className={styles.author}>
+                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/48223561?v=4" alt="avatar" />
+                <div className={styles.authorInfo}>
+                    <strong>Erika Perciliano</strong>
+                    <span>Software Developer</span>
+                </div>
+            </div>
+
+            <time title='May 11th at 8:13am' dateTime='2022-05-11 08:13:30'>Published 1h ago</time>
+        </header>
+
+        <div className={styles.content}>
+           <p>Hey guys 👋</p> 
+           <p>I just uploaded another project to my portfolio. It's a project I did at NLW Return, a Rocketseat event. The name of the project is DoctorCare 🚀</p>
+           <p>👉 <a href=''>jane.design/doctorcare</a></p>
+           <p>
+                <a href=''>#newproject</a> {''}
+                <a href=''>#nlw</a> {''}
+                <a href=''>#rocketseat</a> {''} 
+            </p>
+        </div>
+    </article>
   )
 }
