64c29503a254e7acbb54b9f166bc72f719a7b460
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-02-23 23:09:53 +0000

    add props to the Post Component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,37 @@ import styles from './App.module.css'
 import './global.css'
 import { Sidebar } from './components/Sidebar'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/48223561?v=4',
+      name: 'Erika Perciliano',
+      role: 'Software Engineer', 
+    },
+    content: [
+      { type: 'paragraph', content: 'Hey guys 👋' }, 
+      { type: 'paragraph', content:'I just uploaded another project to my portfolio. Its a project I did at NLW Return, a Rocketseat event. The name of the project is DoctorCare 🚀' },
+      { type: 'link', content: '👉 jane.design/doctorcare' },
+     ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'John Doe',
+      role: 'Software Engineer', 
+    },
+    content: [
+      { type: 'paragraph', content: 'Hey guys 👋' }, 
+      { type: 'paragraph', content:'I just uploaded another project to my portfolio. Its a project I did at NLW Return, a Rocketseat event. The name of the project is DoctorCare 🚀' },
+      { type: 'link', content: '👉 jane.design/doctorcare' },
+     ],
+    publishedAt: new Date('2022-05-10 20:00:00'),
+  }
+]
+
 function App() {
   return (
     <div>
@@ -13,14 +44,18 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar/>
         <main>
-          <Post 
-            author="Erika Perciliano" 
-            content="Aliquip laboris dolor magna laborum dolore consequat labore amet sit esse minim officia nulla duis. Minim ea non cillum adipisicing dolore elit aute mollit Lorem commodo id tempor fugiat. Consequat ex mollit cillum ipsum commodo nulla laboris. Proident pariatur irure laboris velit cillum aliquip id. Amet voluptate est anim sunt pariatur in. Proident non aliquip nostrud ipsum aliqua consequat aliquip labore do laboris amet aliquip."
-          />
-          <Post 
-            author="Raquel Ferreira" 
-            content="Aliquip laboris dolor magna laborum dolore consequat labore amet sit esse minim officia nulla duis. Minim ea non cillum adipisicing dolore elit aute mollit Lorem commodo id tempor fugiat. Consequat ex mollit cillum ipsum commodo nulla laboris. Proident pariatur irure laboris velit cillum aliquip id. Amet voluptate est anim sunt pariatur in. Proident non aliquip nostrud ipsum aliqua consequat aliquip labore do laboris amet aliquip."
-          />
+          {
+            posts.map(post => {
+              return(
+                <Post 
+                  key={post.id} 
+                  author={post.author}
+                  content={post.content}
+                  publishedAt={post.publishedAt}
+                />
+              )
+            })
+          }
         </main>
       </div>
     </div>
